4fc4465773edd630db1424d33bca2506acee4d90
Samuel Felipe <samfelgar@gmail.com>
2023-11-17 05:30:07 -0300

    Add layouts

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const route = useRoute();
+
+useHead({
+  title: 'Filos',
+});
+</script>
+
+<template>
+  <div class="flex flex-col md:flex-row gap-4 p-2">
+    <NuxtLoadingIndicator color="blue"/>
+    <AppSidebar class="hidden md:block"/>
+    <Card class="w-full">
+      <template #title>{{ route.meta.title ?? 'Filos' }}</template>
+      <template #content>
+        <slot/>
+      </template>
+    </Card>
+  </div>
+</template>
---
+++ layouts/guest.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+useHead({
+  title: 'Filos'
+})
+</script>
+
+<template>
+  <slot/>
+</template>
+
+<style scoped>
+
+</style>
