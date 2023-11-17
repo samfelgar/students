09ae47b4665e2cb9ec9cbb13295ed02dc003c943
Samuel Felipe <samfelgar@gmail.com>
2023-11-17 05:29:30 -0300

    Add required packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: {enabled: false},
+    modules: [
+        'nuxt-primevue',
+        '@nuxtjs/supabase'
+    ],
+    css: [
+        '~/assets/css/main.css',
+        'primevue/resources/themes/lara-light-blue/theme.css',
+        'primeicons/primeicons.css',
+    ],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+    primevue: {
+        cssLayerOrder: 'tailwind-base, primevue, tailwind-utilities'
+    }
 })
