a61e526fea8457edbdd8ec404833f4f4b4c7db2c
Samuel Felipe <samfelgar@gmail.com>
2023-11-19 21:55:38 -0300

    Display stats on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
+import FreshmenCard from "~/components/stats/FreshmenCard.vue";
+import VeteransCard from "~/components/stats/VeteransCard.vue";
+import TotalStudentsCard from "~/components/stats/TotalStudentsCard.vue";
+import ClassesStudentsCountCard from "~/components/stats/ClassesStudentsCountCard.vue";
+
 definePageMeta({
   title: 'Inicio'
 })
 </script>
 
 <template>
+  <Suspense>
     <div>
-        <h1>Home</h1>
+      <div class="grid grid-flow-row md:grid-flow-col md:grid-cols-3 gap-4">
+        <FreshmenCard/>
+        <VeteransCard/>
+        <TotalStudentsCard/>
+      </div>
+      <ClassesStudentsCountCard class="mt-4"/>
     </div>
+
+
+    <template #fallback>
+      <Skeleton height="10rem" width="30rem"></Skeleton>
+    </template>
+  </Suspense>
 </template>
